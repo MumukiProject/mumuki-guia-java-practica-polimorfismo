ccd51c5e817342483f892f7f835a66d2ee22ffbb
agus@mumuki.org <agus@mumuki.org>
2017-07-11 14:00:32 -0400

    Mumuki Export on 2017-07-11 14:00:32 -0400

--- 00004_Viaje de Valeria/default.java
+++ 00004_Viaje de Valeria/default.java
@@ -2,6 +2,11 @@ class Viaje {
   Transporte transporte;
   Alojamiento alojamiento;
   
+  public Viaje(Transporte transporte, Alojamiento alojamiento) {
+    this.transporte = transporte;
+    this.alojamiento = alojamiento;
+  }
+  
   public int cuantoCuesta() {
     return transporte.costoTotal() + alojamiento.arancel();
   }
