16265f86a2e4c0c82719a1eecffb37170b6d73fc
agus@mumuki.org <agus@mumuki.org>
2017-07-13 15:53:48 -0400

    Mumuki Export on 2017-07-13 15:53:48 -0400

--- 00007_Mascota Virtual - Parte 3/test.java
+++ 00007_Mascota Virtual - Parte 3/test.java
@@ -1,1 +1,27 @@
-a
+MascotaVirtual mascota;
+@Before
+public void before() {
+  mascota = new MascotaVirtual();
+}
+
+@Test
+public void CuandoJuegaContentaEsMasMadura() {
+  mascota.setEstado(new Contenta());
+  mascota.jugar();
+  Assert.assertEquals(2, mascota.getNivelDeMadurez());
+}
+
+@Test
+public void CuandoJuega2VecesEstandoContentaSePoneHambrienta() {
+  mascota.setEstado(new Contenta());
+  mascota.jugar();
+  mascota.jugar();
+  Assert.assertEquals("Hambrienta", mascota.getEstado().getClass().getName());
+}
+
+@Test
+public void SiJuegaEstandoAburridaSePoneContenta() {
+  mascota.setEstado(new Aburrida());
+  mascota.jugar();
+  Assert.assertEquals("Contenta", mascota.getEstado().getClass().getName());
+}
