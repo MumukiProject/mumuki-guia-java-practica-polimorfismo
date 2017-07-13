1b38cc28a0793a15a5217f33ddcc87f9fb854d83
agus@mumuki.org <agus@mumuki.org>
2017-07-13 16:37:09 -0400

    Mumuki Export on 2017-07-13 16:37:09 -0400

--- 00003_Monedero - Parte 3/test.java
+++ 00003_Monedero - Parte 3/test.java
@@ -1,12 +1,32 @@
 CuentaCorriente cc;
 CajaDeAhorro ca;
-CajaDeAhorroEnDolares cad;
 
 @Before
 public void before() {
   cc = new CuentaCorriente();
   ca = new CajaDeAhorro();
   cad = new CajaDeAhorroEnDolares();
+}
+
+@Test
+public void giroAlDescubierto() {
+  cc.depositar(200);
+  cc.extraer(300);
+  Assert.assertEquals(-100, cc.getSaldo());
+}
+
+@Test
+public void extraerMasDeLoQueSePuede() {
+  ca.depositar(200);
+  ca.extraer(300);
+  Assert.assertEquals(200, ca.getSaldo());
+}
+
+@Test
+public void extraer() {
+  ca.depositar(200);
+  ca.extraer(100);
+  Assert.assertEquals(100, ca.getSaldo());
 }
 
 @Test
