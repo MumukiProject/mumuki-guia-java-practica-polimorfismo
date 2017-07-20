2ae5a5762a79dccfca4ec03f5fc530739d3d313e
agus@mumuki.org <agus@mumuki.org>
2017-07-20 10:15:28 -0400

    Mumuki Export on 2017-07-20 10:15:28 -0400

--- 00008_Metegol - Parte 1/test.java
+++ 00008_Metegol - Parte 1/test.java
@@ -2,17 +2,17 @@ Metegol metegol = new Metegol();
 @Test
 public void conAntiguedad2() {
   metegol.setAntiguedad(2);
-  Assert.assertEquals(22, metegol.habilidad())
+  Assert.assertEquals(22, metegol.habilidad());
 }
 
 @Test
 public void conAntiguedad3() {
   metegol.setAntiguedad(3);
-  Assert.assertEquals(33, metegol.habilidad())
+  Assert.assertEquals(33, metegol.habilidad());
 }
 
 @Test
 public void conAntiguedad0() {
   metegol.setAntiguedad(0);
-  Assert.assertEquals(0, metegol.habilidad())
+  Assert.assertEquals(0, metegol.habilidad());
 }
