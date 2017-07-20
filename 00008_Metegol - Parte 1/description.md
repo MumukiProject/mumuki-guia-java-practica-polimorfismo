001bd3723e9c6ca4ef64714fad429aab40b0863d
agus@mumuki.org <agus@mumuki.org>
2017-07-20 09:59:52 -0400

    Mumuki Export on 2017-07-20 09:59:52 -0400

--- 00008_Metegol - Parte 1/description.md
+++ 00008_Metegol - Parte 1/description.md
@@ -4,7 +4,7 @@ Analicemos la situación:
 
 * El `Metegol` consta de 11 `Jugador`es como el fútbol de verdad
 * De cada `Metegol` se sabe su `antigüedad`
-* Por ahora solo sabemos que la `cantidadDeGoles` de un metegol es 11 * su `antigüedad`
+* Por ahora solo sabemos que la `habilidad` de un metegol es 11 * su `antigüedad`
 
 
 
--- 00008_Metegol - Parte 1/test.java
+++ 00008_Metegol - Parte 1/test.java
@@ -1,2 +1,18 @@
+Metegol metegol = new Metegol();
 @Test
-public void 
+public void conAntiguedad2() {
+  metegol.setAntiguedad(2);
+  Assert.assertEquals(22, metegol.habilidad())
+}
+
+@Test
+public void conAntiguedad3() {
+  metegol.setAntiguedad(3);
+  Assert.assertEquals(33, metegol.habilidad())
+}
+
+@Test
+public void conAntiguedad0() {
+  metegol.setAntiguedad(0);
+  Assert.assertEquals(0, metegol.habilidad())
+}
