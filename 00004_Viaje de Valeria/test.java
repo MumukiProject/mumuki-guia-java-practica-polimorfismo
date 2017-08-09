17e7f2e86c77d5276fa588911b46e39b1234a43f
franco@mumuki.org <franco@mumuki.org>
2017-08-09 12:32:24 -0400

    Mumuki Export on 2017-08-09 12:32:24 -0400

--- 00004_Viaje de Valeria/test.java
+++ 00004_Viaje de Valeria/test.java
@@ -3,19 +3,15 @@ Viaje viaje;
 @Test
 public void conMicroYDepartamento() {
   Micro micro = new Micro();
-  Departamento departamento = new Departamento();
-  departamento.setIncluyeDesayuno(false);
+  Departamento departamento = new Departamento(false);
   viaje = new Viaje(micro, departamento);
   Assert.assertEquals(1500, viaje.cuantoCuesta());
 }
 
 @Test
 public void conAvionYHotel() {
-  Avion avion = new Avion();
-  avion.setCostoPasaje(500);
-  Hotel hotel = new Hotel();
-  hotel.setCantidadDeNoches(4);
-  hotel.setCantidadDeEstrellas(5);
+  Avion avion = new Avion(500);
+  Hotel hotel = new Hotel(4, 5);
   viaje = new Viaje(avion, hotel);
   Assert.assertEquals(1020, viaje.cuantoCuesta());
 }
@@ -23,8 +19,7 @@ public void conAvionYHotel() {
 @Test
 public void conBicicletaYDepartamentoSinDesayuno() {
   Bicicleta bicicleta = new Bicicleta();
-  Departamento departamento = new Departamento();
-  departamento.setIncluyeDesayuno(false);
+  Departamento departamento = new Departamento(false);
   viaje = new Viaje(bicicleta, departamento);
   Assert.assertEquals(1000, viaje.cuantoCuesta());
 }
@@ -32,8 +27,7 @@ public void conBicicletaYDepartamentoSinDesayuno() {
 @Test
 public void conBicicletaYDepartamentoConDesayuno() {
   Bicicleta bicicleta = new Bicicleta();
-  Departamento departamento = new Departamento();
-  departamento.setIncluyeDesayuno(true);
+  Departamento departamento = new Departamento(true);
   viaje = new Viaje(bicicleta, departamento);
   Assert.assertEquals(2000, viaje.cuantoCuesta());
 }
