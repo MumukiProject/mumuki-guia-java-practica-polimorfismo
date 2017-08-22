45be5232c35b15808c570ea61ad8e7cb6968eec8
franco@mumuki.org <franco@mumuki.org>
2017-08-22 16:56:16 -0400

    Mumuki Export on 2017-08-22 16:56:16 -0400

--- 00007_Jugando también se madura/test.java
+++ 00007_Jugando también se madura/test.java
@@ -1,47 +1,40 @@
-MascotaVirtual mascota;
-@Before
-public void before() {
-  mascota = new MascotaVirtual();
-}
-
 @Test
-public void cuandoNaceEstaHambrienta() {
-  Assert.assertEquals("Hambrienta", mascota.getEstado().getClass().getName());
-}
-
-@Test
-public void cuandoComeSePoneContenta() {
-  Assert.assertEquals("Hambrienta", mascota.getEstado().getClass().getName());
+public void cuando_come_se_pone_contenta() {
+  MascotaVirtual mascota = new MascotaVirtual();
   mascota.comer();
-  Assert.assertEquals("Contenta", mascota.getEstado().getClass().getName());
+  Assert.assertTrue(mascota.estaContenta());
 }
 
 @Test
-public void cuandoCome2VecesEstandoContentaSePoneAburrida() {
+public void cuando_come_dos_veces_se_cansa() {
+  MascotaVirtual mascota = new MascotaVirtual();
   mascota.comer();
   mascota.comer();
+  Assert.assertTrue(mascota.estaCansada());
+}
+
+@Test
+public void cuando_come_deja_de_estar_hambrienta() {
+  MascotaVirtual mascota = new MascotaVirtual();
   mascota.comer();
-  Assert.assertEquals("Aburrida", mascota.getEstado().getClass().getName());
+  Assert.assertFalse(mascota.estaHambrienta());
 }
 
 @Test
-public void cuandoJuegaContentaEsMasMadura() {
-  mascota.setEstado(new Contenta());
-  mascota.jugar();
-  Assert.assertEquals(2, mascota.getNivelDeMadurez());
+public void cuando_inicia_esta_hambrienta() {
+  MascotaVirtual mascota = new MascotaVirtual();
+  Assert.assertTrue(mascota.estaHambrienta());
 }
 
 @Test
-public void cuandoJuega2VecesEstandoContentaSePoneHambrienta() {
-  mascota.setEstado(new Contenta());
-  mascota.jugar();
-  mascota.jugar();
-  Assert.assertEquals("Hambrienta", mascota.getEstado().getClass().getName());
+public void cuando_inicia_no_esta_contenta() {
+  MascotaVirtual mascota = new MascotaVirtual();
+  Assert.assertFalse(mascota.estaContenta());
 }
 
+
 @Test
-public void siJuegaEstandoAburridaSePoneContenta() {
-  mascota.setEstado(new Aburrida());
-  mascota.jugar();
-  Assert.assertEquals("Contenta", mascota.getEstado().getClass().getName());
+public void cuando_inicia_no_esta_cansada() {
+  MascotaVirtual mascota = new MascotaVirtual();
+  Assert.assertFalse(mascota.estaCansada());
 }
