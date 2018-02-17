e2b402373d7cdf12ff1a7b1053f22f488658d998
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-17 13:42:34 -0500

    Mumuki Export on 2018-02-17 13:42:34 -0500

--- 00005_Jugo de fruta/test.java
+++ 00005_Jugo de fruta/test.java
@@ -7,10 +7,12 @@ public void una_manzana_da_80_ml_de_jugo() {
   Assert.assertEquals(80, manzana.jugo());
 }
 
+@Test
 public void una_naranja_da_105_ml_de_jugo() {
   Assert.assertEquals(105, naranja.jugo());
 }
 
+@Test
 public void un_pomelo_da_130_ml_de_jugo() {
   Assert.assertEquals(120, pomelo.jugo());
 }
