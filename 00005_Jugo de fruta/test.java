8baa00610b5009cd1aafb21049a38876d6f38252
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-17 13:39:43 -0500

    Mumuki Export on 2018-02-17 13:39:43 -0500

--- 00005_Jugo de fruta/test.java
+++ 00005_Jugo de fruta/test.java
@@ -1,4 +1,16 @@
+Manzana manzana;
+Pomelo pomelo;
+Naranja naranja;
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void una_manzana_da_80_ml_de_jugo() {
+  Assert.asserEquals(80, manzana.jugo());
+}
+
+public void una_naranja_da_105_ml_de_jugo() {
+  Assert.asserEquals(105, naranja.jugo());
+}
+
+public void un_pomelo_da_130_ml_de_jugo() {
+  Assert.asserEquals(120, pomelo.jugo());
 }
