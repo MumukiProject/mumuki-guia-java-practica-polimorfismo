51f13cc3d6e6725d4d489cbc78f291442f9adbac
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-17 14:07:47 -0500

    Mumuki Export on 2018-02-17 14:07:47 -0500

--- 00005_Jugo de fruta/test.java
+++ 00005_Jugo de fruta/test.java
@@ -35,14 +35,23 @@ public void un_pomelo_es_acido() {
 @Test
 public void un_jugo_de_manzana_no_tiene_mucha_vitamina_c() {
   Jugo jugoDeManzana = new Jugo(manzana);
+  Assert.assertFalse(jugoDeManzana.tieneMuchaVitaminaC());
 }
 
 @Test
 public void un_jugo_de_pomelo_tiene_mucha_vitamina_c() {
   Jugo jugoDePomelo = new Jugo(pomelo);
+  Assert.assertTrue(jugoDePomelo.tieneMuchaVitaminaC());
 }
 
 @Test
 public void un_jugo_de_naranja_tiene_205_ml_de_jugo_total() {
   Jugo jugoDeNaranja = new Jugo(naranja);
+  Assert.assertEquals(205, jugoDeNaranja.jugoTotal());
+}
+
+@Test
+public void un_jugo_de_pomelo_tiene_230_ml_de_jugo_total() {
+  Jugo jugoDePomelo = new Jugo(pomelo);
+  Assert.assertEquals(205, jugoDePomelo.jugoTotal());
 }
