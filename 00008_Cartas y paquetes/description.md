f7d84b2f57836f757a73fedd2f692102bce98539
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-17 16:10:24 -0500

    Mumuki Export on 2018-02-17 16:10:24 -0500

--- 00008_Cartas y paquetes/description.md
+++ 00008_Cartas y paquetes/description.md
@@ -1,6 +1,6 @@
 Ahora, ¡enviemos algunas cosas! :envelope: :package:
 
-Queremos enviar cartas y paquetes y, en principio, nos vamos a concentrar en las propiedades físicas de nuestras encomiendas. Un paquete sabe el peso de su contenido y el volumen en centímetros cúbicos (cm³). El paquete `esPequenio` si su volumen es menor a 2000cm³ y `esLiviano` si su peso es menor a 200g. Su peso se determina agregándole 50g, que es lo que pesa la caja, al peso del contenido. 
+Queremos enviar cartas y paquetes y, en principio, nos vamos a concentrar en las propiedades físicas de nuestras encomiendas. Un paquete sabe el peso de su contenido y el volumen en centímetros cúbicos (cm³). El paquete `esPequenio` si su volumen es menor a 2000cm³ y `esLiviano` si su peso es menor a 200g. Su `peso` se determina agregándole 50g, que es lo que pesa la caja, al peso del contenido. 
 
 Por otro lado, la carta conoce la cantidad de líneas escritas que hay en ella. Siempre `esPequenio`, y su peso se calcula como la cantidad de líneas * 0.1 + 2g del peso del papel.
 
--- 00008_Cartas y paquetes/test.java
+++ 00008_Cartas y paquetes/test.java
@@ -1,4 +1,41 @@
+Paquete paqueteChico = new Paquete(1900, 120);
+Paquete paqueteGrande = new Paquete(2100, 180);
+Carta cartaCorta = new Carta(10);
+Carta cartaLarga = new Carta(500);
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void un_paquete_de_volumen_menor_a_2000_cm3_es_pequenio() {
+  Assert.assertTrue(paqueteChico.esPequenio());
+}
+
+@Test
+public void un_paquete_de_volumen_mayor_a_2000_cm3_no_es_pequenio() {
+  Assert.assertFalse(paqueteGrande.esPequenio());
+}
+
+@Test
+public void el_peso_de_un_paquete_es_el_de_su_contenido_mas_el_de_la_caja() {
+  Assert.assertEquals(170, paqueteChico.peso());
+}
+
+@Test
+public void un_paquete_de_peso_menor_a_200_gramos_es_liviano() {
+  Assert.assertTrue(paqueteChico.esLiviano());
+}
+
+@Test
+public void un_paquete_de_peso_mayor_a_200_gramos_no_es_liviano() {
+  Assert.assertFalse(paqueteGrande.esLiviano());
+}
+
+@Test
+public void las_cartas_siempre_son_pequenias() {
+  Assert.assertTrue(cartaCorta.esPequenio());
+  Assert.assertTrue(cartaLarga.esPequenio());
+}
+
+@Test
+public void el_peso_de_una_carta_depende_de_su_cantidad_de_lineas_y_el_peso_del_papel() {
+  Assert.assertEquals(3, cartaCorta.peso());
+  Assert.assertEquals(53, cartaLarga.peso());
 }
