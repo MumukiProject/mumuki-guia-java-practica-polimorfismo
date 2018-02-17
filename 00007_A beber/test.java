f9d99281d206194c8f7ed1ade79f8bcf739aef91
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-17 15:31:18 -0500

    Mumuki Export on 2018-02-17 15:31:18 -0500

--- 00007_A beber/test.java
+++ 00007_A beber/test.java
@@ -1,4 +1,90 @@
+Manzana manzana = new Manzana();
+Pomelo pomelo = new Pomelo();
+Naranja naranja = new Naranja();
+JugoSimple jugoDeManzana = new JugoSimple(manzana);
+JugoSimple jugoDePomelo = new JugoSimple(pomelo);
+JugoSimple jugoDeNaranja = new JugoSimple(naranja);
+JugoMixto jugoDeManzanaYNaranja = new JugoMixto(manzana, naranja);
+JugoMixto jugoDeNaranjaYPomelo = new JugoMixto(naranja, pomelo);
+JugoMixto jugoDeManzanaYPomelo = new JugoMixto(manzana, pomelo);
+
+  
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(false);
+public void una_manzana_da_80_ml_de_jugo() {
+  Assert.assertEquals(80, manzana.jugo());
+}
+
+@Test
+public void una_naranja_da_105_ml_de_jugo() {
+  Assert.assertEquals(105, naranja.jugo());
+}
+
+@Test
+public void un_pomelo_da_130_ml_de_jugo() {
+  Assert.assertEquals(130, pomelo.jugo());
+}
+
+@Test
+public void una_manzana_no_es_acida() {
+  Assert.assertFalse(manzana.esAcida());
+}
+
+@Test
+public void una_naranja_es_acida() {
+  Assert.assertTrue(naranja.esAcida());
+}
+
+@Test
+public void un_pomelo_es_acido() {
+  Assert.assertTrue(pomelo.esAcida());
+}
+
+@Test
+public void un_jugo_de_manzana_no_tiene_mucha_vitamina_c() {
+  Assert.assertFalse(jugoDeManzana.tieneMuchaVitaminaC());
+}
+
+@Test
+public void un_jugo_de_pomelo_tiene_mucha_vitamina_c() {
+  Assert.assertTrue(jugoDePomelo.tieneMuchaVitaminaC());
+}
+
+@Test
+public void un_jugo_de_naranja_tiene_205_ml_de_jugo_total() {
+  Assert.assertEquals(205, jugoDeNaranja.jugoTotal());
+}
+
+@Test
+public void un_jugo_de_pomelo_tiene_230_ml_de_jugo_total() {
+  Assert.assertEquals(230, jugoDePomelo.jugoTotal());
+}
+
+@Test
+public void un_jugo_mixto_de_manzana_y_naranja_no_tiene_mucha_vitamina_c() {
+  Assert.assertFalse(jugoDeManzanaYNaranja.tieneMuchaVitaminaC());
+}
+
+@Test
+public void un_jugo_mixto_de_naranja_y_pomelo_tiene_mucha_vitamina_c() {
+  Assert.assertTrue(jugoDeNaranjaYPomelo.tieneMuchaVitaminaC());
+}
+
+@Test
+public void un_jugo_mixto_de_manzana_y_pomelo_tiene_mucha_vitamina_c() {
+  Assert.assertTrue(jugoDeManzanaYPomelo.tieneMuchaVitaminaC());
+}
+
+@Test
+public void un_jugo_mixto_de_manzana_y_naranja_tiene_235_ml_de_jugo_total() {
+  Assert.assertEquals(235, jugoDeManzanaYNaranja.jugoTotal());
+}
+
+@Test
+public void un_jugo_mixto_de_naranja_y_pomelo_tiene_285_ml_de_jugo_total() {
+  Assert.assertEquals(285, jugoDeNaranjaYPomelo.jugoTotal());
+}
+
+@Test
+public void un_jugo_mixto_de_manzana_y_pomelo_tiene_260_ml_de_jugo_total() {
+  Assert.assertEquals(260, jugoDeManzanaYPomelo.jugoTotal());
 }
