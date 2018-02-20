eee1586b48111e12817521bc330b6ef41862f65a
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-20 08:21:11 -0500

    Mumuki Export on 2018-02-20 08:21:11 -0500

--- 00010_Con urgencia/extra.java
+++ 00010_Con urgencia/extra.java
@@ -1,1 +1,17 @@
 /*...previousSolution...*/
+
+class Ciudad {
+  int kilometros;
+  
+  Ciudad(int ubicacionEnKilometros) {
+    kilometros = ubicacionEnKilometros;
+  }
+  
+  int kilometros() {
+    return kilometros;
+  }
+  
+  int distancia(Ciudad otraCiudad) {
+    return (this.kilometros() - otraCiudad.kilometros());
+  }
+}
