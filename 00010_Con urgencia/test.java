1780e43395dc7b20434ee584373ad63a27f5c9ab
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-20 08:25:31 -0500

    Mumuki Export on 2018-02-20 08:25:31 -0500

--- 00010_Con urgencia/test.java
+++ 00010_Con urgencia/test.java
@@ -4,25 +4,25 @@ Carta carta = new Carta(10);
 Ciudad marDelTuyu = new Ciudad(349);
 Ciudad santaClara = new Ciudad(403);
 Ciudad ushuaia = new Ciudad(3112);
-EnvioUrgente envioDesdeUshuaia = new EnvioUrgente(ushuaia);
+EnvioUrgente envioDesdeMarDelTuyu = new EnvioUrgente(marDelTuyu);
 
 @Test
 public void un_paquete_pequenio_siempre_se_puede_enviar_de_forma_urgente() {
- Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(paqueteChico, santaClara));
- Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(paqueteGrande, marDelTuyu));
+ Assert.assertTrue(envioDesdeMarDelTuyu.puedeEnviar(paqueteChico, santaClara));
+ Assert.assertTrue(envioDesdeMarDelTuyu.puedeEnviar(paqueteGrande, ushuaia));
 }
 
 @Test
 public void una_carta_siempre_se_puede_enviar_de_forma_urgente() {
- Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(carta, ushuaia));
+ Assert.assertTrue(envioDesdeMarDelTuyu.puedeEnviar(carta, ushuaia));
 }
 
 @Test
 public void el_costo_de_envio_urgente_de_un_paquete_depende_de_su_peso_y_un_valor_fijo() {
- Assert.assertEquals(5170, envioDesdeUshuaia.costo(paqueteChico, santaClara)); 
+ Assert.assertEquals(5170, envioDesdeMarDelTuyu.costo(paqueteChico, ushuaia)); 
 }
 
 @Test
 public void el_costo_de_envio_urgente_de_una_carta_depende_de_su_peso_y_un_valor_fijo() {
- Assert.assertEquals(5012, envioDesdeUshuaia.costo(carta, santaClara));
+ Assert.assertEquals(5012, envioDesdeMarDelTuyu.costo(carta, ushuaia));
 }
