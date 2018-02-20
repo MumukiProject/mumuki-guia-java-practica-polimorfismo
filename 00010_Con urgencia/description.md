23cd51e7c4c5c0b422e960957d1b2e9196e91384
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-20 07:45:20 -0500

    Mumuki Export on 2018-02-20 07:45:20 -0500

--- 00010_Con urgencia/description.md
+++ 00010_Con urgencia/description.md
@@ -1,5 +1,5 @@
 No todo lo que queramos enviar va a ir tan cerca ni con tanta tranquilidad: también tendremos envíos urgentes. :scream: 
 
-Un `EnvioUrgente` se puede enviar siempre, pero dada su naturaleza, es muy caro: cuesta $5000 más el peso en gramos del distribuible. Obviamente, al igual que en un envío corta distancia, tiene ciudades origen y destino.
+Un `EnvioUrgente` se puede enviar siempre, pero dada su naturaleza, su costo es muy alto: cuesta $5000 más el peso en gramos del distribuible. Obviamente, al igual que en un envío corta distancia, conoce su ciudad origen.
 
 > ¡Vamos, rápido! :running: Creá la clase `EnvioUrgente` con sus métodos y luego _enviá_ tu solución. :stuck_out_tongue_winking_eye: 
--- 00010_Con urgencia/test.java
+++ 00010_Con urgencia/test.java
@@ -1,4 +1,28 @@
+Paquete paqueteChico = new Paquete(1900, 120);
+Paquete paqueteGrande = new Paquete(2100, 180);
+Carta carta = new Carta(10);
+Ciudad marDelTuyu = new Ciudad(349);
+Ciudad santaClara = new Ciudad(403);
+Ciudad ushuaia = new Ciudad(3112);
+EnvioUrgente envioDesdeUshuaia = new EnvioUrgente(ushuaia);
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void un_paquete_pequenio_siempre_se_puede_enviar_de_forma_urgente() {
+ Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(paqueteChico, santaClara));
+ Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(paquetGrande, marDelTuyu));
+}
+
+@Test
+public void una_carta_siempre_se_puede_enviar_de_forma_urgente() {
+ Assert.assertTrue(envioDesdeUshuaia.puedeEnviar(carta, ushuaia));
+}
+
+@Test
+public void el_costo_de_envio_urgente_de_un_paquete_depende_de_su_peso_y_un_valor_fijo() {
+ Assert.assertEquals(5170, envioDesdeUshuaia.costo(paqueteChico, santaClara)); 
+}
+
+@Test
+public void el_costo_de_envio_urgente_de_una_carta_depende_de_su_peso_y_un_valor_fijo() {
+ Assert.assertEquals(5012, envioDesdeUshuaia.costo(carta, santaClara));
 }
